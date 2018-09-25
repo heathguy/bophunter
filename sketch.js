315f98da94391989953ad85754fc564cf4252768
Heath <43174742+heathguy@users.noreply.github.com>
2018-09-25 14:03:39 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var pship;
 var eships_level1[];
 
-var bullet;
+var bullets[];
 
 function setup() {
   createCanvas(400, 400);
@@ -14,18 +14,38 @@ function setup() {
 	}
 }
 
+function keyPressed() {
+	pbullet = new Bullet(pship.pos.x,pship.pos.y,0,-3,0);
+	bullets.push(pbullet);
+}
+
 function draw() {
   background(51);
 	pship.drawShip();
 	pship.moveShip();
-	
-	bullet = new Bullet(pship.pos.x,pship.pos.y,0,-3,0);
 	
 	for(var i = 0; i < eships_level1.length; i++) {
 		eships_level1[i].drawShip();
 		eships_level1[i].moveShip();
 	}
 	
-	bullet.drawBullet();
-	bullet.moveBullet();
+	for(var j = 0; j < bullets.length; j++) {
+		bullets[j].drawBullet();
+		bullets[j].moveBullet();
+		
+		if(bullets[j].enemy) {
+			boolean hitPlayer = bullets[j].checkHitPlayer(pship);
+			if(hitPlayer) {
+				pship.life -= 10;
+			}
+		}
+		else {
+			for(var k = eships_level1 - 1; k > 0; k--) {
+				boolean hitEnemy = bullets[j].checkHitEnemy(eships_level1[k]);
+				if(hitEnemy) {
+					eships_level1[k].splice(k,1);
+				}
+			}
+		}
+	}
 }
