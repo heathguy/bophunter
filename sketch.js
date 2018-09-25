d1e703a8e3bb5476f0dd6c0133b8d89f6e32df41
Heath <43174742+heathguy@users.noreply.github.com>
2018-09-25 13:48:05 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,14 +12,14 @@ function setup() {
 		var eship = new Enemyship(random(0,width-20),random(-20,-100),3,0,1);
 		eships_level1.push(eship);
 	}
-	
-	bullet = new Bullet(pship.pos.x,pship.pos.y,0,-3,0);
 }
 
 function draw() {
   background(51);
 	pship.drawShip();
 	pship.moveShip();
+	
+	bullet = new Bullet(pship.pos.x,pship.pos.y,0,-3,0);
 	
 	for(var i = 0; i < eships_level1.length; i++) {
 		eships_level1[i].drawShip();
