1b61a04924b20a0fa743704396bf32a71a7449fa
Heath <43174742+heathguy@users.noreply.github.com>
2018-09-25 12:16:04 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,20 @@
 var pship;
 
+var eship1;
+
 function setup() {
   createCanvas(400, 400);
 	pship = new Playership();
 	pship.respawn();
+	
+	eship1 = new Enemyship(random(0,width),-50,3,0,1);
 }
 
 function draw() {
   background(51);
 	pship.drawShip();
 	pship.moveShip();
+	
+	eship1.drawShip();
+	eship1.moveShip();
 }
