070a4babc86a8beb283d210bde338cbb9f0c57f8
Heath <43174742+heathguy@users.noreply.github.com>
2018-09-25 13:44:22 -0400

    Create bullet.js

---
+++ bullet.js
@@ -0,0 +1,22 @@
+class Bullet {
+  constructor(x,y,velx,vely,enemy) {
+    this.pos = createVector(x,y);
+    this.vel = createVector(velx,vely);
+    this.enemy = enemy;
+    }
+    
+  showBullet() {
+    noStroke();
+    if(this.enemy) {
+      fill(255,0,0);
+     }
+     else{
+      fill(20,255,255);
+      }
+      point(this.pos.x, this.pos.y);
+    }
+    
+  moveBullet() {
+    this.pos.add(this.vel);
+    }
+}
